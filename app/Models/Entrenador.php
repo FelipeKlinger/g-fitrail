f8c01d441d742af8540d995a44cf99c9825c8394
root <root@daw-dev-gui>
2026-01-25 17:16:42 +0000

    Model Entrenador migrado y creado, para la relacion con sede

---
+++ app/Models/Entrenador.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Entrenador extends Model
+{
+    protected $fillable = [
+        "nombre",
+        "email",
+        "telefono",
+        "direccion",
+        "especialidad",
+        "password",
+    ];
+}
---
+++ database/migrations/2026_01_25_164824_create_entrenadors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void //up() se utiliza para definir las operaciones de migración
+    {
+        Schema::create('entrenadors', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre", 100);
+            $table->string("email", 100)->unique();
+            $table->integer("telefono");
+            $table->string("direccion", 150);
+            $table->enum("especialidad", [
+                'Musculación',
+                'CrossFit',
+                'Funcional',
+                'Yoga',
+                'Rehabilitación'
+            ]);
+            $table->string("password", 20)->nullable(); //nullable para permitir valores nulos
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entrenadors');
+    }
+};
