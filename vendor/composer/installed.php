02ebeccadd3039d1127c8cd1193440c4fe6fd7e2
jukl915 <jukl915@vidalibarraquer.net>
2026-03-28 21:03:58 +0100

    rama personal

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2415614f46d852cf4be18d53a5c9730bafec4433',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -452,9 +452,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2415614f46d852cf4be18d53a5c9730bafec4433',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
